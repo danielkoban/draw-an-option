6b8f4bd156a600eaf130a264b30972497bff82fb
Daniel Koban <danielkoban@gmail.com>
2022-04-29 11:54:47 +0200

    Add Drawing component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <AddItem />
   <ItemList />
+  <Drawing />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AddItem from "./components/AddItem.vue";
 import ItemList from "./components/ItemList.vue";
+import Drawing from "./components/Drawing.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     AddItem,
     ItemList,
+    Drawing,
   },
 });
 </script>
---
+++ src/components/Drawing.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>{{ drawnItem }}</h2>
+  <button @click="drawItem" :disabled="!disabled">draw</button>
+</template>
+
+<script>
+import { defineComponent, ref, computed } from "vue";
+import store from "../store";
+
+export default defineComponent({
+  setup() {
+    const { items } = store;
+    const drawnItem = ref("");
+
+    const drawItem = () => {
+      drawnItem.value =
+        items.value[Math.floor(Math.random() * items.value.length)];
+    };
+
+    const disabled = computed(() => !!items.value.length);
+
+    return {
+      drawnItem,
+      items,
+      drawItem,
+      disabled,
+    };
+  },
+});
+</script>
+
+<style>
+</style>
